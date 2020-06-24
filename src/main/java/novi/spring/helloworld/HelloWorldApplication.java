1c2c17819f467d94de039677c538f31274516571
CTTY NUL <66295799+CTTY-NUL@users.noreply.github.com>
2020-06-24 20:41:32 +0200

    Initial commit

---
+++ src/main/java/novi/spring/helloworld/HelloWorldApplication.java
@@ -0,0 +1,24 @@
+package novi.spring.helloworld;
+
+import com.what3words.javawrapper.What3WordsV3;
+import com.what3words.javawrapper.request.Coordinates;
+import com.what3words.javawrapper.response.ConvertTo3WA;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+
+		What3WordsV3 api = new What3WordsV3("V8UG7ICS");
+
+		ConvertTo3WA words = api.convertTo3wa(new Coordinates(51.508344, -0.12549900))
+				.language("en")
+				.execute();
+		System.out.println("Words: " + words);
+
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
