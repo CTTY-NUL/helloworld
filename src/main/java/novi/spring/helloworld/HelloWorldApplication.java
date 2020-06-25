090d1c91875d5cb36a62a8402513a0c4864fd345
CTTY NUL <66295799+CTTY-NUL@users.noreply.github.com>
2020-06-25 12:07:46 +0200

    uitbreiding 4

--- src/main/java/novi/spring/helloworld/HelloWorldApplication.java
+++ src/main/java/novi/spring/helloworld/HelloWorldApplication.java
@@ -1,8 +1,8 @@
 package novi.spring.helloworld;
 
-import com.what3words.javawrapper.What3WordsV3;
-import com.what3words.javawrapper.request.Coordinates;
-import com.what3words.javawrapper.response.ConvertTo3WA;
+//import com.what3words.javawrapper.What3WordsV3;
+//import com.what3words.javawrapper.request.Coordinates;
+//import com.what3words.javawrapper.response.ConvertTo3WA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,14 +11,14 @@ public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 
-		//dit stukje is alleen voor what3words
-		What3WordsV3 api = new What3WordsV3("V8UG7ICS");
-
-		ConvertTo3WA words = api.convertTo3wa(new Coordinates(51.508344, -0.12549900))
-				.language("en")
-				.execute();
-		System.out.println("Words: " + words);
-		//einde stukje voor what3words
+//		//dit stukje is alleen voor what3words
+//		What3WordsV3 api = new What3WordsV3("V8UG7ICS");
+//
+//		ConvertTo3WA words = api.convertTo3wa(new Coordinates(51.508344, -0.12549900))
+//				.language("en")
+//				.execute();
+//		System.out.println("Words: " + words);
+//		//einde stukje voor what3words
 
 
 		SpringApplication.run(HelloWorldApplication.class, args);
